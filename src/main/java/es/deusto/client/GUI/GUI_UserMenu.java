3b3e5e92eb607818ca8cd6e34d13e70ce20d09b2
Asier97 <asier.lopezdeuralde@opendeusto.es>
2018-04-17 11:57:46 +0200

    Small change

--- src/main/java/es/deusto/client/GUI/GUI_UserMenu.java
+++ src/main/java/es/deusto/client/GUI/GUI_UserMenu.java
@@ -1,6 +1,6 @@
 package main.java.es.deusto.client.GUI;
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.EventQueue;;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
