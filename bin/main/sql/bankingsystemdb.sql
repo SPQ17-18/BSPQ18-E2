d05f2e8a23e63ac11622af264224c467754b101b
pabsmartin <pablomartin@opendeusto.es>
2018-05-14 08:21:14 +0200

    minor changes

---
+++ bin/main/sql/bankingsystemdb.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-04-2018 a las 14:10:05
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bankingsystemdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `account_number int(10) UNSIGNED NOT NULL,
+  `dni` char(9) NOT NULL,
+  `amount` float NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `creation_date` date NOT NULL,
+  `expiration_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `dni_user1` char(9) NOT NULL,
+  `dni_user2` char(9) NOT NULL,
+  `amount` float NOT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `surname` varchar(30) NOT NULL,
+  `birthday` date NOT NULL,
+  `creditcard_number` int(11) NOT NULL,
+  `dni` char(9) NOT NULL,
+  `address` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`dni_user1`,`dni_user2`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `dni` (`dni`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `user` (`dni`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
